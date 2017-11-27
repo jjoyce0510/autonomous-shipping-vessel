7c7d02b8e7883c88d80d4ab618caa41dc3f1fe9f
William Markley <willjmarkley@gmail.com>
2017-11-26 20:51:38 -0500

    Added angle logic

--- src/main/python/wrappers/gps/Coordinates.py
+++ src/main/python/wrappers/gps/Coordinates.py
@@ -21,3 +21,26 @@ class Coordinates:
 	def calculateDistanceTo(self, coordinates):
 		# Calculate distance to another set of coordinates
 		return math.sqrt( math.pow((self.lat - coordinates.getLat()),2) + math.pow((self.lon - coordinates.getLon()),2) )
+
+	def calculateAngleFrom(self, coordinates):
+		# Calculate angle from self to another set of coordinates
+		# Angle represented as degrees from true north
+		'''
+		Longitude: East (+), West (-) from GPS
+		Latitude:  North (+), South (-) from GPS
+		
+				 E
+			     |
+			S <--+--> N
+				 |
+				 W
+				 
+		Y-axis is longitude
+		X-axis is latitude
+			
+		'''
+		latDifference = coordinates.getLat() - self.lat
+		lonDifference = coordinates.getLon() - self.lon
+		# atan2 returns the correct quadrant and accounts for directly East and directly West
+		return math.degrees(math.atan2(lonDifference,latDifference))
+
