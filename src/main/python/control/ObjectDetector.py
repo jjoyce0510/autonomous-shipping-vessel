0dcd2ef1a53ff5bbacbefd0db23cdd9c3b349900
John Joyce <jjoyce6@nd.edu>
2017-12-02 13:00:40 -0500

    small updates

--- src/main/python/control/ObjectDetector.py
+++ src/main/python/control/ObjectDetector.py
@@ -1,20 +1,22 @@
 import cv2
 
 class ObjectDetector:
-    lidar = None
+	lidar = None
 
-    Lower = (20, 80, 5)
-    Upper = (80, 255, 185)
+	Lower = (20, 80, 5)
+	Upper = (80, 255, 185)
 
-    ball_x = None
-    ball_y = None
-    ball_radius = None
+	ball_x = None
+	ball_y = None
+	ball_radius = None
 
-    def __init__(self, lidar=None):
-        self.lidar = lidar
+	detectedObject = None
+
+	def __init__(self, lidar=None):
+		self.lidar = lidar
         
 
-    def detectObject(self, frame):
+	def detectObject(self, frame):
 
 		vertical_img = cv2.flip(frame, 1 )
 		# blur frame using Gaussian blur
@@ -31,7 +33,6 @@ class ObjectDetector:
 
 		# find the contours in the mask and initialize the current (x, y) center of the ball
 		contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-		center = None
 
 		# only proceed if there are more than one contours
 		if len(contours)>0:
@@ -47,14 +48,23 @@ class ObjectDetector:
 				# draw the circle and centroid on the frame
 				cv2.circle(frame, (int(self.ball_x), int(self.ball_y)), int(self.ball_radius), (0, 255, 255), 2)
 				cv2.circle(frame, center, 5, (0, 0, 255), -1)
+				self.detectObjectWithCenter(center, self.ball_radius)
 
 		else:
 			self.ball_x = None
 			self.ball_y = None
 			self.ball_radius = None
 
+		# Decide what to do here.
+		# If object is detected, check the lidar.
+
 		return frame
 
+	def detectObjectWithCenter(self, center, radius):
+		# Check the lidar, create a profile for the object, if it has lidar, we can put distance on it.
+		# Set as global variable and allow the driver to request if there are any objects.
+		pass
 
-
+	def getObject(self):
+		return self.detectedObject
 
