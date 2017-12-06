ae18d5114324852e79410701710db5d777b77c0a
Will Markley <willmarkley@users.noreply.github.com>
2017-12-05 23:26:12 -0500

    Update boot.sh

--- src/main/shell/boot.sh
+++ src/main/shell/boot.sh
@@ -6,12 +6,9 @@ sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 cd /home/pi/autonomous-shipping-vessel
 
 touch boot.log
-echo "Start 1 try" >> boot.log
-python -m src.main.python.Main >> boot.log
-echo "Completed 1 try" >> boot.log
-exit 1
-#until python -m src.main.python.Main >> boot.log
-#do
-#    sleep 0.1
-#    echo "=========================================" >> boot.log
-#done
+echo "==== Starting ====" >> boot.log
+until python -m src.main.python.Main >> boot.log
+do
+    sleep 30
+    echo "=========================================" >> boot.log
+done
